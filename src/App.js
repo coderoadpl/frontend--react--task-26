95139954fda5eab7cb35d9a4c5b53ec8625fe531
Mateusz Choma <kontakt@coderoad.pl>
2021-06-14 20:31:20 +0200

    `Button` cumponent added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
+
+export const Button = (props) => {
+  const { label } = props
+
+  return (
+    <button>
+      {label}
+    </button>
+  )
+}
 
 export const App = () => {
   return (
     <div>
       CodeRoad APP
+      <Button
+        label={'Click me'}
+      />
     </div>
   )
 }
